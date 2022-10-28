1b146f868184c817762d565a02dbb458bb360c94
Fatih Dokuyucu <fdokuyucu@gmail.com>
2022-10-28 18:03:24 -0400

    Deneme

---
+++ force-app/main/default/lwc/modalParent/modalParent.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+
+export default class ModalParent extends LightningElement {
+    showModal = false;
+    message;
+
+    clickHandler(){
+        this.showModal =true;
+    }
+    modalHandler(event){
+        this.showModal= false;
+        console.log(event);
+        this.message= event.detail;
+    }
+}
---
+++ force-app/main/default/lwc/sliderParent/sliderParent.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class SliderParent extends LightningElement {
+    changeSlider(){
+        this.template.querySelector
+        ('c-slider-component').resetSlider();
+    }
+}
